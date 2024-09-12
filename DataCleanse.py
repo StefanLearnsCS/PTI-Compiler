f0d915d7f018bcdbf1e9161b95f2c97f22d410b2
Stefan <stefancool00@gmail.com>
2024-09-11 21:34:16 -0700

    Switched pdf reader module, datacleanse function completed.

---
+++ DataCleanse.py
@@ -0,0 +1,13 @@
+
+def dataCleanse(rawText):
+    lines = rawText.strip().split('\n')
+
+    partsMap = {}
+
+    for line in lines:
+        parts = line.split()
+        
+        if parts[0].isdigit():
+            partsMap[int(parts[0])] = {'drawing': parts[2], 'issue': parts[-1]}
+
+    return partsMap
--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
 from ReadList import read_allparts
 from GetDirectory import get_directory
+from DataCleanse import dataCleanse
 
 def main():
     allPartsPath = get_directory()
     allPartsDrawing = input("File Name Containing All Parts List (i.e. PM-#####-TA): ")
 
-    print(read_allparts(allPartsPath, allPartsDrawing))
+    allPartsRawText = read_allparts(allPartsPath, allPartsDrawing)
+    
+    allPartsDictionaryUnsorted = dataCleanse(allPartsRawText)
+    allPartsDictionary = {k: allPartsDictionaryUnsorted[k] for k in sorted(allPartsDictionaryUnsorted)}
+    
+    print(allPartsDictionary)
 
 main()
