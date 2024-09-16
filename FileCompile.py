3c6b217792813e17ffff5a6fd09dede83941ee71
Stefan <stefancool00@gmail.com>
2024-09-15 18:47:04 -0700

    SPI Parts done

--- FileCompile.py
+++ FileCompile.py
@@ -11,6 +11,7 @@ def fileCompile(source, ssPath, ppPath, itemMap, soNumber):
         pdfName = itemNumber + '.pdf'
         dxfName = itemNumber + '.dxf'
         multiDxfName = itemNumber + '_'
+        spiPath = "C:\\Users\\stefa\\OneDrive\\Desktop\\PTI Test\\20216\\20216 SPI"
 
         if soNumber in itemNumber:
             if 's' in issueCode:
@@ -24,6 +25,22 @@ def fileCompile(source, ssPath, ppPath, itemMap, soNumber):
                 for root, dirs, files in os.walk(source):
                     for file in files:
                         if (pdfName in file) or (dxfName in file) or (multiDxfName in file):
+                            source_file = os.path.join(root, file)
+                            dest_file = os.path.join(ppPath, file)
+                            shutil.copy2(source_file, dest_file)
+
+        if 'spi' in itemNumber:
+            if 's' in issueCode:
+                for root, dirs, files in os.walk(spiPath):
+                    for file in files:
+                        if (itemNumber in file):
+                            source_file = os.path.join(root, file)
+                            dest_file = os.path.join(ssPath, file)
+                            shutil.copy2(source_file, dest_file)
+            if 'p' in issueCode:
+                for root, dirs, files in os.walk(spiPath):
+                    for file in files:
+                        if (itemNumber in file):
                             source_file = os.path.join(root, file)
                             dest_file = os.path.join(ppPath, file)
                             shutil.copy2(source_file, dest_file)
