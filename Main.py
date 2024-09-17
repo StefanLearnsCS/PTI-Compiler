03f0156738e04e1dcb44400c0f7949b2deaed9b6
Stefan <stefancool00@gmail.com>
2024-09-16 17:49:12 -0700

    Gasket compiler added

--- Main.py
+++ Main.py
@@ -9,6 +9,21 @@ from ItemList import itemListCreator
 def main():
     soNumber = input("SO Number (i.e. 20216): ")
     tankType = input("What type of package (Tank, Clamp, or UA): ")
+    
+    gasket = False
+    pressboard = False
+    ssal = False
+    StS = False
+
+    if tankType == 'Tank' or tankType == 'tank':
+        if input("Would you like a Gasket package (Issue # P5G)? (Y/N): ") in ['y', 'Y']:
+            gasket = True
+        if input("Would you like a Pressboard package (Issue # P8)? (Y/N): ") in ['y', 'Y']:
+            pressboard = True
+        if input("Would you like a Stainless/Aluminum package (Issue # P3 & P4AL)? (Y/N): ") in ['y', 'Y']:
+            ssal = True
+        if input("Would you like a Structural Steel package (Issue # P4 & P14)? (Y/N): ") in ['y', 'Y']:
+            StS = True
 
     allPartsPath = get_directory()
     allPartsDrawing = input("File Name Containing All Parts List (i.e. PM-#####-TA): ")
@@ -28,6 +43,11 @@ def main():
 
     itemListCreator(allPartsPath, allPartsDrawing, 'Steel Supplier Parts', newMainFolder, soNumber, 'SS')
     itemListCreator(allPartsPath, allPartsDrawing, 'Parts - SAP', newMainFolder, soNumber, 'PP')
+
+    if gasket:
+        gaFolderPath, newMainFolder = folderCreation(soNumber, tankType, 'GA')
+        fileCompile(allPartsPath, gaFolderPath, 'p5g', allPartsDictionaryWithParents, soNumber)
+        itemListCreator(allPartsPath, allPartsDrawing, '(GASKETS)', newMainFolder, soNumber, 'GA')
 
     input("Success! Press any key to close window.")
 
